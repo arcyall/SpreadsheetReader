3aba730aa1a0db758219d44e0477253d9a5bc412
arcyall <105183977+arcyall@users.noreply.github.com>
2022-09-12 22:47:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SpreadsheetReader
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            string? input;
+
+            while (true)
+            {
+                Console.WriteLine("Enter filename with file extension: ");
+                input = Console.ReadLine();
+
+                if (File.Exists($"./{input}"))
+                {
+                    Console.WriteLine("File found");
+                    break;
+                }
+                Console.WriteLine("File not found");
+            }
+
+            SpreadsheetManager.ParseFile(input);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ SpreadsheetManager.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.Sqlite;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetReader
+{
+    public static class SpreadsheetManager
+    {
+        public static void ParseFile(string input)
+        {
+            string filetype = input.Substring(input.LastIndexOf('.') + 1);
+            string name = input[..(input.LastIndexOf('.') - 1)];
+            Console.WriteLine($"Provided filename: {name}");
+            Console.WriteLine($"Provided filetype: {filetype}");
+
+            switch (filetype)
+            {
+                //TODO: support more file types such as csv, ods
+                case "xlsx":
+                    Console.WriteLine("Processing excel sheet");
+                    using (var package = new ExcelPackage($"./{input}"))
+                    {
+                        for (int i = 0; i < package.Workbook.Worksheets.Count; ++i)
+                        {
+                            Console.WriteLine($"Processing {package.Workbook.Worksheets[i]}");
+                            ProcessSheet(name, package.Workbook.Worksheets[i]);
+                            Console.WriteLine($"Processed {package.Workbook.Worksheets[i].Name}");
+                        }
+                    }
+                    Console.WriteLine("File fully loaded into db");
+                    break;
+
+                default:
+                    Console.WriteLine("Unsupported filetype");
+                    break;
+            }
+        }
+
+        public static void ProcessSheet(string name, ExcelWorksheet input)
+        {
+            //using var connection = new SqliteConnection($"Data Source={name}.db");
+            throw new NotImplementedException();
+        }
+    }
+}
